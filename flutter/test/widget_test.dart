469726b27698d2e3437388179b676f3d63df6fef
iori tobeta <15eb1137@gmail.com>
2022-10-04 21:33:02 +0900

    [feat] Input text

--- flutter/test/widget_test.dart
+++ flutter/test/widget_test.dart
@@ -1,7 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 // import 'test_data.dart';
 
 void main() {
-  test('', () async {});
+  testWidgets('TextField Learning test', (tester) async {
+    await tester.pumpWidget(wrap(const TextField()));
+    await tester.enterText(find.byType(TextField), 'あ');
+    await tester.enterText(find.byType(TextField), 'い');
+    await tester.pump();
+    expect(find.text('あい'), findsNothing);
+    expect(find.text('い'), findsOneWidget);
+  });
+}
+
+Widget wrap(Widget child) {
+  return MaterialApp(home: Material(child: child));
 }
