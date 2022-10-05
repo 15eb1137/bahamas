79f9a011be5317ad70cc561a0206dd9b06b7eb36
iori tobeta <15eb1137@gmail.com>
2022-10-05 22:25:56 +0900

    TextField Learning test

--- flutter/test/widget_test.dart
+++ flutter/test/widget_test.dart
@@ -6,6 +6,12 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('TextField Learning test', (tester) async {
     await tester.pumpWidget(wrap(const TextField()));
+    expect(tester.testTextInput.isVisible, false);
+
+    await tester.tap(find.byType(TextField));
+    await tester.pump();
+    expect(tester.testTextInput.isVisible, true);
+
     await tester.enterText(find.byType(TextField), 'あ');
     await tester.enterText(find.byType(TextField), 'い');
     await tester.pump();
