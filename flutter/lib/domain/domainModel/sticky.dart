985f6308073e370a091f90d23434fb5eb8de14b3
iori tobeta <15eb1137@gmail.com>
2023-01-06 14:26:39 +0900

    set sticky class

--- flutter/lib/domain/domainModel/sticky.dart
+++ flutter/lib/domain/domainModel/sticky.dart
@@ -1,1 +1,47 @@
-class Sticky {}
+import 'value/sticky_color.dart';
+import 'value/sticky_font_size.dart';
+import 'value/sticky_id.dart';
+import 'value/sticky_last_edit.dart';
+import 'value/sticky_state.dart';
+import 'value/sticky_text.dart';
+
+class Sticky {
+  final StickyId id;
+  StickyText _text;
+  StickyFontSize _fontSize;
+  StickyLastEdit _lastEdit;
+  StickyColor _color;
+  StickyState _state;
+
+  Sticky(
+      {required this.id,
+      required StickyText text,
+      required StickyFontSize fontSize,
+      required StickyLastEdit lastEdit,
+      required StickyColor color,
+      required StickyState state})
+      : _text = text,
+        _fontSize = fontSize,
+        _lastEdit = lastEdit,
+        _color = color,
+        _state = state;
+
+  StickyText get text => _text;
+  StickyFontSize get fontSize => _fontSize;
+  StickyLastEdit get lastEdit => _lastEdit;
+  StickyColor get color => _color;
+  StickyState get state => _state;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(other, this) || (other is Sticky && other.id == id);
+
+  @override
+  int get hashCode => runtimeType.hashCode ^ id.hashCode;
+
+  void changeText(StickyText newText) => _text = newText;
+  void changeFontSize(StickyFontSize newFontSize) => _fontSize = newFontSize;
+  void changeLastEdit(StickyLastEdit newLastEdit) => _lastEdit = newLastEdit;
+  void changeColor(StickyColor newColor) => _color = newColor;
+  void changeState(StickyState newState) => _state = newState;
+}
