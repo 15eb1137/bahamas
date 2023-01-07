a2cd16248eba64e27b5cf949779b307187966d0e
iori tobeta <15eb1137@gmail.com>
2023-01-07 14:34:41 +0900

    remove unused param

--- flutter/lib/domain/domainService/search/search_domain_service.dart
+++ flutter/lib/domain/domainService/search/search_domain_service.dart
@@ -1,11 +1,13 @@
 import '../../domainModel/search/value/search_id.dart';
-import 'search_repository.dart';
 
 class SearchDomainSearvice {
-  final SearchRepository _repository;
+  // final SearchRepository _repository;
 
-  SearchDomainSearvice({required SearchRepository repository})
-      : _repository = repository;
+  SearchDomainSearvice(
+      // {required SearchRepository repository}
+      )
+  // : _repository = repository
+  ;
 
   @deprecated
   bool isExistId(SearchId id) {
