2d096f6440b3bb66f357f760adac9b8e9b137917
iori tobeta <15eb1137@gmail.com>
2023-01-07 13:43:21 +0900

    set sticies operators

--- flutter/lib/domain/domainModel/sticky/stickies.dart
+++ flutter/lib/domain/domainModel/sticky/stickies.dart
@@ -1,11 +1,21 @@
+import 'package:flutter/foundation.dart';
+
 import 'sticky.dart';
 
 class Stickies {
-  final List<Sticky> children;
+  final List<Sticky> _children;
 
-  Stickies({required this.children});
+  Stickies({required List<Sticky> children}) : _children = children;
 
-  int get length => children.length;
+  List<Sticky> get children => _children;
+  int get length => _children.length;
 
-  Sticky operator [](int index) => children[index];
+  Sticky operator [](int index) => _children[index];
+
+  @override
+  bool operator ==(Object other) =>
+      other is Stickies && listEquals(other._children, _children);
+
+  @override
+  int get hashCode => runtimeType.hashCode ^ _children.hashCode;
 }
