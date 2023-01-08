0d79600cc14299b09e138a18dd10d38e1679d19c
iori tobeta <15eb1137@gmail.com>
2023-01-09 02:32:47 +0900

    remove widget test

--- flutter/test/widget_test.dart
+++ flutter/test/widget_test.dart
@@ -1,25 +1,23 @@
-import 'package:bahamas/presentation/widget/sticky/sticky_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_test/flutter_test.dart';
 
 // import 'test_data.dart';
 
 void main() {
-  testWidgets('TextField Learning test', (tester) async {
-    await tester.pumpWidget(wrap(const StickyTextField(isNew: true)));
-    expect(tester.testTextInput.isVisible, false);
+  // testWidgets('TextField Learning test', (tester) async {
+  //   await tester.pumpWidget(wrap(const StickyTextField(isNew: true)));
+  //   expect(tester.testTextInput.isVisible, false);
 
-    await tester.tap(find.byType(TextField));
-    await tester.pump();
-    expect(tester.testTextInput.isVisible, true);
+  //   await tester.tap(find.byType(TextField));
+  //   await tester.pump();
+  //   expect(tester.testTextInput.isVisible, true);
 
-    await tester.enterText(find.byType(TextField), 'あ');
-    await tester.enterText(find.byType(TextField), 'い');
-    await tester.pump();
-    expect(find.text('あい'), findsNothing);
-    expect(find.text('い'), findsOneWidget);
-  });
+  //   await tester.enterText(find.byType(TextField), 'あ');
+  //   await tester.enterText(find.byType(TextField), 'い');
+  //   await tester.pump();
+  //   expect(find.text('あい'), findsNothing);
+  //   expect(find.text('い'), findsOneWidget);
+  // });
   // testWidgets('MemoMenuButton test', (tester) async {
   //   await tester.pumpWidget(wrap(const MemoMenuButton()));
   //   expect(find.byIcon(Icons.more_vert), findsOneWidget);
