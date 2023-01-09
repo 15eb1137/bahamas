82a086d2abd815b52e85b0f404e9a3a8bff475e6
iori tobeta <15eb1137@gmail.com>
2023-01-09 12:28:38 +0900

    add isar collection file

---
+++ flutter/lib/infrastructure/sticky/sticky_isar_collection.dart
@@ -0,0 +1,39 @@
+import 'package:isar/isar.dart';
+
+import '../../domain/domainModel/sticky/value/sticky_state.dart';
+
+part 'sticky_isar_collection.g.dart';
+
+@Collection(accessor: 'stickies')
+class StickyIsarCollection {
+  late String id;
+
+  Id get isarId => fastHash(id);
+
+  late String text;
+
+  late int fontSize;
+
+  @Index()
+  late DateTime lastEdit;
+
+  late int color;
+
+  @enumerated
+  late StickyStateType state;
+
+  int fastHash(String string) {
+    var hash = 0xcbf29ce484222325;
+
+    var i = 0;
+    while (i < string.length) {
+      final codeUnit = string.codeUnitAt(i++);
+      hash ^= codeUnit >> 8;
+      hash *= 0x100000001b3;
+      hash ^= codeUnit & 0xFF;
+      hash *= 0x100000001b3;
+    }
+
+    return hash;
+  }
+}
